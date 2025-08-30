08b1c7077053d30471b8b42e405776b6c474a767
codot-09 <onabiyev626@gmail.com>
2025-08-30 22:03:05 +0500

    cors

---
+++ src/main/java/com/example/sellify/config/WebConfig.java
@@ -0,0 +1,22 @@
+package com.example.sellify.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig {
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")  // barcha endpointlarga ruxsat
+                        .allowedOriginPatterns("*") // istalgan domen
+                        .allowedMethods("*")        // GET, POST, PUT, DELETE...
+                        .allowedHeaders("*");
+            }
+        };
+    }
+}
